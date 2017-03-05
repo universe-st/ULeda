582bfc9bb817610e3b7d081c5f83f71321ce86d0
Nora0713 <1379026619@qq.com>
2017-03-05 21:07:37 +0800

    mdf

--- app/src/main/java/ecnu/uleda/TaskPostActivity.java
+++ app/src/main/java/ecnu/uleda/TaskPostActivity.java
@@ -1,16 +1,13 @@
 package ecnu.uleda;
 
 //import android.graphics.PorterDuff;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
-import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
@@ -20,26 +17,18 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.tencent.lbssearch.TencentSearch;
 import com.tencent.lbssearch.httpresponse.BaseObject;
 import com.tencent.lbssearch.httpresponse.HttpResponseListener;
 import com.tencent.lbssearch.object.Location;
-import com.tencent.lbssearch.object.param.Address2GeoParam;
 import com.tencent.lbssearch.object.param.Geo2AddressParam;
-import com.tencent.lbssearch.object.result.Address2GeoResultObject;
 import com.tencent.lbssearch.object.result.Geo2AddressResultObject;
 import com.tencent.map.geolocation.TencentLocation;
 import com.tencent.map.geolocation.TencentLocationListener;
 import com.tencent.map.geolocation.TencentLocationManager;
 import com.tencent.map.geolocation.TencentLocationRequest;
-import com.tencent.tencentmap.mapsdk.map.TencentMap;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class TaskPostActivity extends AppCompatActivity {
 
@@ -98,12 +87,6 @@ public class TaskPostActivity extends AppCompatActivity {
     private TencentLocationManager mLocationManager;
     private float latitude = 0;
     private float longitude = 0;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -153,9 +136,6 @@ public class TaskPostActivity extends AppCompatActivity {
                 }.start();
             }
         });
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     protected void init() {
@@ -192,21 +172,8 @@ public class TaskPostActivity extends AppCompatActivity {
                     public void onStatusUpdate(String s, int i, String s1) {
                     }
                 });
-
                 //Toast.makeText(TaskPostActivity.this,""+longitude ,Toast.LENGTH_SHORT).show();
                 getNearAdd();
-
-               /* Geo2AddressParam param = new Geo2AddressParam().location(new Location().lat(latitude).lng(longitude));
-                param.get_poi(true);
-
-                Geo2AddressResultObject oj = (Geo2AddressResultObject) object;
-                String result = "坐标转地址：lat:" + String.valueOf(latitude) + "  lng:" +
-                        String.valueOf(longitude) + "\n\n";
-                if (oj.result != null) {
-                    Log.v("demo", "address:" + oj.result.address);
-                    result += oj.result.address;
-                }
-                Toast.makeText(TaskPostActivity.this, result, Toast.LENGTH_SHORT).show();*/
             }
 
         });
@@ -214,19 +181,10 @@ public class TaskPostActivity extends AppCompatActivity {
 
     private void getNearAdd() {
 
-        TencentSearch mtencentSearch = new TencentSearch(
-                getApplicationContext());
-
-        //通过前面获取的经纬度在这里会用到，因为查找周围是需要根据Location对象来进行查找
+        TencentSearch mtencentSearch = new TencentSearch(getApplicationContext());
         Geo2AddressParam param = new Geo2AddressParam().location(new Location().lat(latitude).lng(longitude));
-
-        //设置为true才能获取到周围的建筑，默认为false
         param.get_poi(true);
-
-        /** 因为是获取周围建筑，所以也会有监听，需要实现他的2个方法onSuccess(int arg0, Header[] arg1, BaseObject arg2)                    和onFailure(int arg0, Header[] arg1, String arg2,Throwable arg3) */
-
         mtencentSearch.geo2address(param, new HttpResponseListener() {
-
             @Override
             public void onSuccess(int i, BaseObject baseObject) {
                 if (baseObject != null) {
@@ -236,8 +194,7 @@ public class TaskPostActivity extends AppCompatActivity {
                     param.get_poi(true);
 
                     Geo2AddressResultObject oj = (Geo2AddressResultObject) baseObject;
-                    String result = oj.result.address;//
-                    // = "坐标转地址：lat:" + String.valueOf(latitude) + "  lng:" +String.valueOf(longitude) + "\n\n";
+                    String result = oj.result.address;
                     /*if (oj.result != null) {
                         Log.v("demo", "address:" + oj.result.address);
                         result += oj.result.address;
@@ -251,39 +208,6 @@ public class TaskPostActivity extends AppCompatActivity {
             public void onFailure(int i, String s, Throwable throwable) {
 
             }
-
-
-
-           /* @Override
-            public void onSuccess(int arg0, PreferenceActivity.Header[] arg1, BaseObject object) {
-                // TODO Auto-generated method stub
-                if (object != null) {
-
-
-                    Geo2AddressParam param = new Geo2AddressParam().location(new Location().lat(latitude).lng(longitude));
-                    param.get_poi(true);
-
-                    Geo2AddressResultObject oj = (Geo2AddressResultObject) object;
-                    String result = "坐标转地址：lat:" + String.valueOf(latitude) + "  lng:" +
-                            String.valueOf(longitude) + "\n\n";
-                    if (oj.result != null) {
-                        Log.v("demo", "address:" + oj.result.address);
-                        result += oj.result.address;
-                    }
-                    Toast.makeText(TaskPostActivity.this, result, Toast.LENGTH_SHORT).show();
-                    // result += oj.result.address;
-                }
-
-
-            }
-
-            @Override
-            public void onFailure(int arg0, PreferenceActivity.Header[] arg1, String arg2,
-                                  Throwable arg3) {
-                // TODO Auto-generated method stub
-
-            }
-*/
         });
     }
 
@@ -310,41 +234,6 @@ public class TaskPostActivity extends AppCompatActivity {
         });
     }
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-/*    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("TaskPost Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
-    }*/
 
     private class MyTextWatcher implements TextWatcher {
         @Override
