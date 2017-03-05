b2295f9f00c5022b0216c9991c14b5e36549d848
Nora0713 <1379026619@qq.com>
2017-03-05 20:59:50 +0800

    获取当前地址

--- app/src/main/java/ecnu/uleda/TaskPostActivity.java
+++ app/src/main/java/ecnu/uleda/TaskPostActivity.java
@@ -1,9 +1,11 @@
 package ecnu.uleda;
 
 //import android.graphics.PorterDuff;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -18,7 +20,13 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.tencent.lbssearch.TencentSearch;
+import com.tencent.lbssearch.httpresponse.BaseObject;
+import com.tencent.lbssearch.httpresponse.HttpResponseListener;
 import com.tencent.lbssearch.object.Location;
 import com.tencent.lbssearch.object.param.Address2GeoParam;
 import com.tencent.lbssearch.object.param.Geo2AddressParam;
@@ -31,26 +39,28 @@ import com.tencent.map.geolocation.TencentLocationRequest;
 import com.tencent.tencentmap.mapsdk.map.TencentMap;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class TaskPostActivity extends AppCompatActivity {
 
     private UserOperatorController mUserOperatorController;
 
-    private Handler mClickHandler=new Handler(){
+    private Handler mClickHandler = new Handler() {
         @Override
-        public void handleMessage(Message msg){
-            if(msg.what==0){
+        public void handleMessage(Message msg) {
+            if (msg.what == 0) {
                 finish();
-            }else{
-                UServerAccessException exception=(UServerAccessException)msg.obj;
-                Toast.makeText(TaskPostActivity.this,"提交任务失败："+exception.getMessage(),Toast.LENGTH_SHORT).show();
+            } else {
+                UServerAccessException exception = (UServerAccessException) msg.obj;
+                Toast.makeText(TaskPostActivity.this, "提交任务失败：" + exception.getMessage(), Toast.LENGTH_SHORT).show();
             }
         }
     };
 
     final private static ArrayList<String> taskPostArray;
+
     static {
-        taskPostArray=new ArrayList<>();
+        taskPostArray = new ArrayList<>();
         taskPostArray.add("全部");
         taskPostArray.add("跑腿代步");
         taskPostArray.add("生活服务");
@@ -60,6 +70,7 @@ public class TaskPostActivity extends AppCompatActivity {
         taskPostArray.add("活动相关");
         taskPostArray.add("其他");
     }
+
     private EditText mEtTitle;
     private Spinner mSpinTag;
     private EditText mEtPrice;
@@ -76,19 +87,22 @@ public class TaskPostActivity extends AppCompatActivity {
     private String mId;
     private String mPpassport;
     private String mTitle;
-    private String mTag="跑腿代步";  //tag任务分类
+    private String mTag = "跑腿代步";  //tag任务分类
     private String mDescription;
     private String mPrice;
     private String mPath;
     private String mActiveTime;
     private String mPosition;
 
-    private  Button buttonTest ;
+    private Button buttonTest;
     private TencentLocationManager mLocationManager;
     private float latitude = 0;
     private float longitude = 0;
-
-
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
 
     @Override
@@ -100,27 +114,23 @@ public class TaskPostActivity extends AppCompatActivity {
 
         mLocationManager = TencentLocationManager.getInstance(this.getApplication());
 
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
 
-        mButtonBack.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        mButtonBack.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 finish();
             }
         });
 
         mEtPrice.addTextChangedListener(new MyTextWatcher());
 
-        //mLocationManager = TencentLocationManager.getInstance(this.getActivity());
-
-
-        mButtonTaskPost.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        mButtonTaskPost.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 SpinnerEvent();
-                if(!getTaskPost())
-                {
-                    Toast.makeText(TaskPostActivity.this, "请输入有效数据～",Toast.LENGTH_SHORT).show();
+                if (!getTaskPost()) {
+                    Toast.makeText(TaskPostActivity.this, "请输入有效数据～", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 //judgeEdittext();
@@ -128,7 +138,7 @@ public class TaskPostActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         try {
-                            ServerAccessApi.postTask(mId,mPpassport,mTitle,mTag,mDescription,mPrice,mPath,mActiveTime,mPosition);
+                            ServerAccessApi.postTask(mId, mPpassport, mTitle, mTag, mDescription, mPrice, mPath, mActiveTime, mPosition);
                             Message message = new Message();
                             message.what = 0;
                             mClickHandler.sendMessage(message);
@@ -143,23 +153,25 @@ public class TaskPostActivity extends AppCompatActivity {
                 }.start();
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    protected void init(){
-        mButtonBack=(Button) findViewById(R.id.button_task_post_back);
-        mButtonTaskPost=(Button)findViewById(R.id.button_task_post);
+    protected void init() {
+        mButtonBack = (Button) findViewById(R.id.button_task_post_back);
+        mButtonTaskPost = (Button) findViewById(R.id.button_task_post);
         mEtTitle = (EditText) findViewById(R.id.task_post_title);
-        mEtPrice=(EditText)findViewById(R.id.task_post_payment);
-        mEtdestination = (EditText)findViewById(R.id.task_post_destination);
-        mEtActiveTime = (EditText)findViewById(R.id.task_post_activeTime);
-        mEtStart = (EditText)findViewById(R.id.task_post_start);
-        mEtdestination = (EditText)findViewById(R.id.task_post_description);
-        mEtDescription = (EditText)findViewById(R.id.task_post_description);
+        mEtPrice = (EditText) findViewById(R.id.task_post_payment);
+        mEtdestination = (EditText) findViewById(R.id.task_post_destination);
+        mEtActiveTime = (EditText) findViewById(R.id.task_post_activeTime);
+        mEtStart = (EditText) findViewById(R.id.task_post_start);
+        mEtdestination = (EditText) findViewById(R.id.task_post_description);
+        mEtDescription = (EditText) findViewById(R.id.task_post_description);
 
 
-        buttonTest= (Button)findViewById(R.id.button_test);
-        buttonTest.setOnClickListener(new View.OnClickListener()
-        {
+        buttonTest = (Button) findViewById(R.id.button_test);
+        buttonTest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mLocationManager.requestLocationUpdates(TencentLocationRequest.create()
@@ -175,44 +187,119 @@ public class TaskPostActivity extends AppCompatActivity {
                         latitude = (float) tencentLocation.getLatitude();
                         longitude = (float) tencentLocation.getLongitude();
                     }
+
                     @Override
                     public void onStatusUpdate(String s, int i, String s1) {
                     }
                 });
 
-                Toast.makeText(TaskPostActivity.this,""+longitude ,Toast.LENGTH_SHORT).show();
+                //Toast.makeText(TaskPostActivity.this,""+longitude ,Toast.LENGTH_SHORT).show();
+                getNearAdd();
+
+               /* Geo2AddressParam param = new Geo2AddressParam().location(new Location().lat(latitude).lng(longitude));
+                param.get_poi(true);
+
+                Geo2AddressResultObject oj = (Geo2AddressResultObject) object;
+                String result = "坐标转地址：lat:" + String.valueOf(latitude) + "  lng:" +
+                        String.valueOf(longitude) + "\n\n";
+                if (oj.result != null) {
+                    Log.v("demo", "address:" + oj.result.address);
+                    result += oj.result.address;
+                }
+                Toast.makeText(TaskPostActivity.this, result, Toast.LENGTH_SHORT).show();*/
             }
 
         });
-
-        /*Geo2AddressParam param = new Geo2AddressParam().location(new Location()
-                .lat(latitude).lng(longitude));
-        param.get_poi(true);
-
-        Geo2AddressResultObject oj = new Geo2AddressResultObject();
-        String result = "坐标转地址：lat:"+String.valueOf(latitude)+"  lng:"+
-                String.valueOf(longitude) + "\n\n";
-        if(oj.result != null){
-            Log.v("demo","address:"+oj.result.address);
-            result += oj.result.address;
-        }*/
     }
 
-    private void SpinnerInit()
-    {
-        mSpinTag=(Spinner)findViewById(R.id.spinner_task_post);
-        taskPostAdapter=new ArrayAdapter<>(this.getApplicationContext(),
-                R.layout.task_post_spinner,taskPostArray);
+    private void getNearAdd() {
+
+        TencentSearch mtencentSearch = new TencentSearch(
+                getApplicationContext());
+
+        //通过前面获取的经纬度在这里会用到，因为查找周围是需要根据Location对象来进行查找
+        Geo2AddressParam param = new Geo2AddressParam().location(new Location().lat(latitude).lng(longitude));
+
+        //设置为true才能获取到周围的建筑，默认为false
+        param.get_poi(true);
+
+        /** 因为是获取周围建筑，所以也会有监听，需要实现他的2个方法onSuccess(int arg0, Header[] arg1, BaseObject arg2)                    和onFailure(int arg0, Header[] arg1, String arg2,Throwable arg3) */
+
+        mtencentSearch.geo2address(param, new HttpResponseListener() {
+
+            @Override
+            public void onSuccess(int i, BaseObject baseObject) {
+                if (baseObject != null) {
+
+
+                    Geo2AddressParam param = new Geo2AddressParam().location(new Location().lat(latitude).lng(longitude));
+                    param.get_poi(true);
+
+                    Geo2AddressResultObject oj = (Geo2AddressResultObject) baseObject;
+                    String result = oj.result.address;//
+                    // = "坐标转地址：lat:" + String.valueOf(latitude) + "  lng:" +String.valueOf(longitude) + "\n\n";
+                    /*if (oj.result != null) {
+                        Log.v("demo", "address:" + oj.result.address);
+                        result += oj.result.address;
+                    }*/
+                    Toast.makeText(TaskPostActivity.this, result, Toast.LENGTH_SHORT).show();
+                    // result += oj.result.address;
+                }
+            }
+
+            @Override
+            public void onFailure(int i, String s, Throwable throwable) {
+
+            }
+
+
+
+           /* @Override
+            public void onSuccess(int arg0, PreferenceActivity.Header[] arg1, BaseObject object) {
+                // TODO Auto-generated method stub
+                if (object != null) {
+
+
+                    Geo2AddressParam param = new Geo2AddressParam().location(new Location().lat(latitude).lng(longitude));
+                    param.get_poi(true);
+
+                    Geo2AddressResultObject oj = (Geo2AddressResultObject) object;
+                    String result = "坐标转地址：lat:" + String.valueOf(latitude) + "  lng:" +
+                            String.valueOf(longitude) + "\n\n";
+                    if (oj.result != null) {
+                        Log.v("demo", "address:" + oj.result.address);
+                        result += oj.result.address;
+                    }
+                    Toast.makeText(TaskPostActivity.this, result, Toast.LENGTH_SHORT).show();
+                    // result += oj.result.address;
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(int arg0, PreferenceActivity.Header[] arg1, String arg2,
+                                  Throwable arg3) {
+                // TODO Auto-generated method stub
+
+            }
+*/
+        });
+    }
+
+    private void SpinnerInit() {
+        mSpinTag = (Spinner) findViewById(R.id.spinner_task_post);
+        taskPostAdapter = new ArrayAdapter<>(this.getApplicationContext(),
+                R.layout.task_post_spinner, taskPostArray);
         taskPostAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         mSpinTag.setAdapter(taskPostAdapter);
     }
 
-    private void SpinnerEvent()
-    {
-        mSpinTag.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
+    private void SpinnerEvent() {
+        mSpinTag.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                mTag=mSpinTag.getSelectedItem().toString();
+                mTag = mSpinTag.getSelectedItem().toString();
                 adapterView.setVisibility(View.VISIBLE);
             }
 
@@ -223,24 +310,61 @@ public class TaskPostActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+/*    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("TaskPost Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }*/
+
     private class MyTextWatcher implements TextWatcher {
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
         }
+
         @Override
         public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            String price=mEtPrice.getText().toString();
-            int posDot=price.indexOf('.');
-            if(posDot<=0)
-                return ;
-            else if(price.length()-posDot-1>2)
-            {
+            String price = mEtPrice.getText().toString();
+            int posDot = price.indexOf('.');
+            if (posDot <= 0)
+                return;
+            else if (price.length() - posDot - 1 > 2) {
                 String string;
-                string = price.substring(0,price.length() - 1);
+                string = price.substring(0, price.length() - 1);
                 mEtPrice.setText(string);
                 mEtPrice.setSelection(string.length());
             }
         }
+
         public void afterTextChanged(Editable s) {
         }
     }
@@ -258,13 +382,12 @@ public class TaskPostActivity extends AppCompatActivity {
         mPosition = "31.2296,121.403";
         //TODO:
 
-        if(mPrice.equals("")||mActiveTime.equals("")||mEtStart.getText().toString().equals("")
-                ||(mEtdestination.getText().toString().equals("")))
+        if (mPrice.equals("") || mActiveTime.equals("") || mEtStart.getText().toString().equals("")
+                || (mEtdestination.getText().toString().equals("")))
             return false;
         //description不能为空???
         return true;
     }
-
 
 
     //TODO: 啊啊啊这个怎么才能不跳转界面乖乖显示Toast
