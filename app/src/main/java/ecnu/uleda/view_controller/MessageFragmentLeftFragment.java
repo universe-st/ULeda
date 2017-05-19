0c75e15d350a6035a6619d1947414f40c43663cc
kuangthree <kuangthree@qq.com>
2017-05-20 00:56:06 +0800

    加入腾讯IM第三方SDK

--- app/src/main/java/ecnu/uleda/view_controller/MessageFragmentLeftFragment.java
+++ app/src/main/java/ecnu/uleda/view_controller/MessageFragmentLeftFragment.java
@@ -1,40 +1,25 @@
 package ecnu.uleda.view_controller;
 
-import android.app.ActivityManager;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
-
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-
 import ecnu.uleda.R;
 import ecnu.uleda.model.ChatMessage;
 import ecnu.uleda.model.Friend;
-import ecnu.uleda.tool.SPUtil;
-import io.rong.imlib.RongIMClient;
-import io.rong.imlib.model.Conversation;
-import io.rong.imlib.model.UserInfo;
+
 
 /**
  * Created by zhaoning on 2017/5/1.
+ * 信息界面左
  */
 
 public class MessageFragmentLeftFragment extends Fragment {
@@ -52,64 +37,23 @@ public class MessageFragmentLeftFragment extends Fragment {
     private List<Friend> userList;
 
     private  String TAG="MFLF";//MessageFragmentLeftFragment is too long(interesting)
+                                        /*I think so
+                                            * -KSS*/
 
     @Override
     public void onCreate(Bundle b) {
         super.onCreate(b);
     }
 
-//    public static TaskMissionFragment getInstance() {
-//        if (mInstance == null) {
-//            synchronized (TaskMissionFragment.class) {
-//                if (mInstance == null) {
-//                    mInstance = new TaskMissionFragment();
-//                }
-//            }
-//        }
-//        return mInstance;
-//    }
+
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.message_fragment_message_fragment,container,false);
-//        vpContent = (ViewPager)view.findViewById(R.id.vp_content);
-//
-////        mFragments.add(mConversationList);//添加会话fragment
-////        mFragments.add(HomeFragment.getInstance());
-//
-//        adapter = new FragmentPagerAdapter(getActivity().getSupportFragmentManager()) {
-//            @Override
-//            public Fragment getItem(int position) {
-//                return mFragments.get(position);
-//            }
-//
-//            @Override
-//            public int getCount() {
-//                return mFragments.size();
-//            }
-//        };
-//        vpContent.setAdapter(adapter);
+        //小柠柠早点睡，别熬夜太晚~代码可以明天写，不急不急~
         return view;
 
     }
-
-
-//    原leftfragment
-//    private void initMessages() {
-//        for (int i = 0; i < 3; i++) {
-//            ChatMessage dyz = new ChatMessage("丁义珍", R.drawable.username);
-//            mChatMessageList.add(dyz);
-//            ChatMessage hlp = new ChatMessage("侯亮平", R.drawable.user2);
-//            mChatMessageList.add(hlp);
-//            ChatMessage ldk = new ChatMessage("李达康", R.drawable.username);
-//            mChatMessageList.add(ldk);
-//            ChatMessage qtw = new ChatMessage("祁同伟", R.drawable.user4);
-//            mChatMessageList.add(qtw);
-//            ChatMessage srj = new ChatMessage("沙瑞金", R.drawable.user5);
-//            mChatMessageList.add(srj);
-//        }
-//    }
-
 
 
     private void replaceFragment(Fragment fragment) {
@@ -119,5 +63,4 @@ public class MessageFragmentLeftFragment extends Fragment {
         transaction.addToBackStack(null);
         transaction.commit();
     }
-
 }
--- app/src/main/java/ecnu/uleda/view_controller/MessageFragmentRightFragment.java
+++ app/src/main/java/ecnu/uleda/view_controller/MessageFragmentRightFragment.java
@@ -13,11 +13,12 @@ import java.util.List;
 import ecnu.uleda.R;
 import ecnu.uleda.model.Contacts;
 import ecnu.uleda.model.Friend;
-import io.rong.imlib.model.UserInfo;
+
 
 
 /**
  * Created by zhaoning on 2017/5/1.
+ * 信息界面右
  */
 
 public class MessageFragmentRightFragment extends Fragment {
@@ -44,24 +45,9 @@ public class MessageFragmentRightFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.message_fragment_right_fragment,container,false);
+        //小柠柠早点睡，别熬夜太晚~代码可以明天写，不急不急~
         return view;
     }
-
-//    private void initContacts(){
-//        for(int i=0;i<2;i++) {
-//            Contacts dyz = new Contacts("丁义珍",R.drawable.username);
-//            contastsList.add(dyz);
-//            Contacts hlp = new Contacts("侯亮平",R.drawable.user2);
-//            contastsList.add(hlp);
-//            Contacts ldk = new Contacts("李达康",R.drawable.username);
-//            contastsList.add(ldk);
-//            Contacts qtw = new Contacts("祁同伟",R.drawable.user4);
-//            contastsList.add(qtw);
-//            Contacts srj = new Contacts("沙瑞金",R.drawable.user5);
-//            contastsList.add(srj);
-//        }
-//    }
-
 
 
 
