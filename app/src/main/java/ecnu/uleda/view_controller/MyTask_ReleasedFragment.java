9d35fdddf6cd27bf763a333d9e258069bce47016
hn <1054673674@qq.com>
2017-09-15 17:39:40 +0800

    扫一扫不能在64位手机运行bug修改，还有我的界面的加载点击功能

--- app/src/main/java/ecnu/uleda/view_controller/MyTask_ReleasedFragment.java
+++ app/src/main/java/ecnu/uleda/view_controller/MyTask_ReleasedFragment.java
@@ -1,5 +1,6 @@
 package ecnu.uleda.view_controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -8,6 +9,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -24,7 +27,10 @@ import ecnu.uleda.exception.UServerAccessException;
 import ecnu.uleda.function_module.ServerAccessApi;
 import ecnu.uleda.function_module.UserOperatorController;
 import ecnu.uleda.model.MyOrder;
+import ecnu.uleda.model.UTask;
 import ecnu.uleda.view_controller.MyOrderAdapter;
+import ecnu.uleda.view_controller.task.activity.TaskDetailsActivity;
+import ecnu.uleda.view_controller.task.adapter.TaskListAdapter;
 
 
 public class MyTask_ReleasedFragment extends Fragment {
@@ -32,13 +38,13 @@ public class MyTask_ReleasedFragment extends Fragment {
     private ListView mlistView;
     private List<MyOrder> releasedList = new ArrayList<>();
     private MyOrderAdapter mMyOrderAdapter;
+    private int Index = 0;
     private Handler handler = new Handler(){
         public void handleMessage(Message msg)
         {
             switch (msg.what)
             {
                 case 1:
-
                         JSONArray jsonArray = (JSONArray)msg.obj;
                         for(int i = 0;i < jsonArray.length();i++)
                         {
@@ -62,7 +68,12 @@ public class MyTask_ReleasedFragment extends Fragment {
                     mMyOrderAdapter.notifyDataSetChanged();
                     mlistView.setAdapter(mMyOrderAdapter);
                         break;
-
+                case 2:
+                    Intent intent = new Intent(getActivity().getApplication(),TaskDetailsActivity.class);
+                    UTask utask = (UTask)msg.obj;
+                    intent.putExtra("UTask",utask);
+                    startActivity(intent);
+                    break;
                 default:
                         break;
             }
@@ -77,30 +88,45 @@ public class MyTask_ReleasedFragment extends Fragment {
         View v=inflater.inflate(R.layout.fragment_my_task__released,parent,false);
         mlistView = (ListView) v.findViewById(R.id.list_view);
         mMyOrderAdapter = new MyOrderAdapter(this.getActivity(),releasedList);
+        loadUserData();
+        mlistView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                if(mlistView.getLastVisiblePosition() == mlistView.getCount() - 1)
+                {
+                    Index++;
+                    loadUserData();
+                }
+            }
 
-        /*releasedList.add(new MyOrder()
-                .setTitle("帮忙重装系统")
-                .setDescription("")
-                .setPrice(BigDecimal.valueOf(10))
-                .setActiveTime(15)
-                .setAuthorCredit(5)
-                .setAuthorID(110)
-                .setAuthorUserName("TonyDanid")
-                .setPath("到理科大楼")
-                .setTag("学习帮助")
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
 
-                .setActiveTime(1260)
-        );*/
-       new Thread(new Runnable() {
+            }
+        });
+        mlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                getTask(position);
+            }
+        });
+        return v;
+    }
+    public void loadUserData()
+    {
+        new Thread(new Runnable() {
             @Override
             public void run() {
                 try
                 {
-                    JSONArray jsonArray = ServerAccessApi.getUserTask(0,0);
-                    Message msg = new Message();
-                    msg.what = 1;
-                    msg.obj = jsonArray;
-                    handler.sendMessage(msg);
+                    JSONArray jsonArray = ServerAccessApi.getUserTask(Index,0);
+                    if(jsonArray.length() > 0)
+                    {
+                        Message msg = new Message();
+                        msg.what = 1;
+                        msg.obj = jsonArray;
+                        handler.sendMessage(msg);
+                    }
                 }catch (UServerAccessException e)
                 {
                     e.printStackTrace();
@@ -109,10 +135,61 @@ public class MyTask_ReleasedFragment extends Fragment {
 
             }
         }).start();
-
-
-
-
-        return v;
+    }
+    public void  getTask(final int position)
+    {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                UTask uTask = new UTask();
+                try {
+                    int index = 0;
+                    int temp = 0;
+                    if(position == 0)
+                    {
+                        index = 0;
+                    }
+                    else
+                    {
+                        if(position % 10 == 0)
+                        {
+                            index = position / 10 - 1;
+                        }
+                        else
+                        {
+                            index = position / 10;
+                        }
+                    }
+                    temp = position - 10 * index;
+                    JSONArray jsonArray = ServerAccessApi.getUserTask(index,0);
+                    JSONObject json = jsonArray.getJSONObject(temp);
+                    uTask.setTitle(json.getString("title"))
+                            .setStatus(Integer.parseInt(json.getString("status")))
+                            .setAuthorID(Integer.parseInt(json.getString("author")))
+                            .setAuthorAvatar(json.getString("authorAvatar"))
+                            .setAuthorUserName(json.getString("authorUsername"))
+                            .setAuthorCredit(Integer.parseInt(json.getString("authorCredit")))
+                            .setTag(json.getString("tag"))
+                            .setDescription(json.getString("description"))
+                            .setPostDate(Long.parseLong(json.getString("postdate")))
+                            .setActiveTime(Long.parseLong(json.getString("activetime")))
+                            .setPath(json.getString("path"))
+                            .setPrice(BigDecimal.valueOf(Double.parseDouble(json.getString("price"))))
+                            .setPostID(json.getString("postID"))
+                            .setTakersCount(Integer.parseInt(json.getString("taker")));
+                    Message msg = new Message();
+                    msg.what = 2;
+                    msg.obj = uTask;
+                    handler.sendMessage(msg);
+                }catch (UServerAccessException e)
+                {
+                    e.printStackTrace();
+                }
+                catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
--- app/src/main/java/ecnu/uleda/view_controller/task/fragment/TaskMissionFragment.java
+++ app/src/main/java/ecnu/uleda/view_controller/task/fragment/TaskMissionFragment.java
@@ -279,15 +279,15 @@ public class TaskMissionFragment extends Fragment {
         mTasksInList = new ArrayList<>();
         mTaskListAdapter = new TaskListAdapter(getActivity(), mTasksInList);
         mTaskListAdapter.setHasStableIds(true);
-        mTaskListView.setAdapter(mTaskListAdapter);
-        mTaskListView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
-        mTaskListView.setRefreshProgressStyle(ProgressStyle.Pacman);
-        mTaskListView.setLoadingMoreProgressStyle(ProgressStyle.Pacman);
-        mTaskListView.setArrowImageView(R.drawable.pull_to_refresh_arrow);
-        mTaskListView.addItemDecoration(new TaskListItemDecoration(getContext(), 8, true));
-        mTaskListAdapter.setOnItemClickListener(new TaskListAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClicked(View v, UTask task) {
+                mTaskListView.setAdapter(mTaskListAdapter);
+                mTaskListView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+                mTaskListView.setRefreshProgressStyle(ProgressStyle.Pacman);
+                mTaskListView.setLoadingMoreProgressStyle(ProgressStyle.Pacman);
+                mTaskListView.setArrowImageView(R.drawable.pull_to_refresh_arrow);
+                mTaskListView.addItemDecoration(new TaskListItemDecoration(getContext(), 8, true));
+                mTaskListAdapter.setOnItemClickListener(new TaskListAdapter.OnItemClickListener() {
+                    @Override
+                    public void onItemClicked(View v, UTask task) {
                 Intent intent = new Intent(getActivity().getApplicationContext(), TaskDetailsActivity.class);
                 intent.putExtra("UTask", task);
                 startActivity(intent);
