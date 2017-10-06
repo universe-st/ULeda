5620e87b2a3b601b5402b0eb991c7bf99031a45f
Nora0713 <1379026619@qq.com>
2017-10-06 21:17:36 +0800

    MFLF continued

---
+++ app/src/main/java/ecnu/uleda/model/Conversation.java
@@ -0,0 +1,49 @@
+package ecnu.uleda.model;
+
+/**
+ * Created by zhaoning on 2017/10/6.
+ */
+
+public class Conversation {
+    private String mConversationuId;
+    private String mConversationName;
+    private String mImageUrl;
+    private String mContent;
+
+    public String getConversationuId() {
+        return mConversationuId;
+    }
+
+    public Conversation setConversationuId(String conversationuId) {
+        mConversationuId = conversationuId;
+        return this;
+    }
+
+    public String getConversationName() {
+        return mConversationName;
+    }
+
+    public Conversation setConversationName(String conversationName) {
+        mConversationName = conversationName;
+        return this;
+    }
+
+    public String getImageUrl() {
+        return mImageUrl;
+    }
+
+    public Conversation setImageUrl(String imageUrl) {
+        mImageUrl = imageUrl;
+        return this;
+    }
+
+    public String getContent() {
+        return mContent;
+    }
+
+    public Conversation setContent(String content) {
+        mContent = content;
+        return this;
+    }
+
+}
--- app/src/main/java/ecnu/uleda/view_controller/message/MessageFragmentLeftFragment.java
+++ app/src/main/java/ecnu/uleda/view_controller/message/MessageFragmentLeftFragment.java
@@ -7,11 +7,19 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.tencent.imsdk.TIMConversation;
+import com.tencent.imsdk.TIMConversationType;
+import com.tencent.imsdk.TIMManager;
+import com.tencent.imsdk.TIMMessage;
+import com.tencent.imsdk.TIMValueCallBack;
+import com.tencent.imsdk.ext.message.TIMConversationExt;
+import com.tencent.imsdk.ext.message.TIMManagerExt;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +36,7 @@ import ecnu.uleda.model.Friend;
 public class MessageFragmentLeftFragment extends Fragment {
 
 
+    private ListView mListView;
     private  String TAG="MFLF";//MessageFragmentLeftFragment is too long(interesting)      -KSS
 
 
@@ -41,7 +50,55 @@ public class MessageFragmentLeftFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.message_fragment_left_fragment,container,false);
+        mListView = (ListView)view.findViewById(R.id.conversation_list);
+
+        initFriendRequest();
+        initConversation();
+
+
         return view;
+
+    }
+
+    void initFriendRequest()
+    {
+
+    }
+
+    void initConversation()
+    {
+        List<TIMConversation> list = TIMManagerExt.getInstance().getConversionList();
+        for(int i=0;i<list.size();i++)
+        {
+            String peerId = list.get(i).getPeer();
+            Log.e(TAG, "initConversation: " +peerId);
+            //获取会话扩展实例
+            TIMConversation con = TIMManager.getInstance().getConversation(TIMConversationType.C2C, peerId);
+            TIMConversationExt conExt = new TIMConversationExt(con);
+
+//获取此会话的消息
+            conExt.getLocalMessage(10, //获取此会话最近的10条消息
+                    null, //不指定从哪条消息开始获取 - 等同于从最新的消息开始往前
+                    new TIMValueCallBack<List<TIMMessage>>() {//回调接口
+                        @Override
+                        public void onError(int code, String desc) {//获取消息失败
+                            //接口返回了错误码code和错误描述desc，可用于定位请求失败原因
+                            //错误码code含义请参见错误码表
+                            Log.d(TAG, "get message failed. code: " + code + " errmsg: " + desc);
+                        }
+
+                        @Override
+                        public void onSuccess(List<TIMMessage> msgs) {//获取消息成功
+                            //遍历取得的消息
+                            for(TIMMessage msg : msgs) {
+                                TIMMessage lastMsg = msg;
+                                //可以通过timestamp()获得消息的时间戳, isSelf()是否为自己发送的消息
+                                Log.e(TAG, "get msg: " + msg.timestamp() + " self: " + msg.isSelf() + " seq: " + msg.getMsg().seq());
+
+                            }
+                        }
+                    });
+        }
 
     }
 
